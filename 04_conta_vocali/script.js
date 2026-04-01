ddfa5a769b6c532ec4bad1860648baaa2f22bfeb
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-04-01 18:46:53 +0200

    04: implementata funzione per contare vocali in una stringa

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,39 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(parola) {
+  let count = 0;
 
+  for (let i = 0; i < parola.length; i++) {
+    const lettera = parola[i];
+
+    // controllo ogni vocale separatamente
+    if (lettera === 'a') {
+      count++;
+    }
+
+    if (lettera === 'e') {
+      count++;
+    }
+
+    if (lettera === 'i') {
+      count++;
+    }
+
+    if (lettera === 'o') {
+      count++;
+    }
+
+    if (lettera === 'u') {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(contaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
